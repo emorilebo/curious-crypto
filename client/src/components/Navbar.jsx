2524a026528eb3ebbf931bfeae2bade5611175e2
Godfrey Lebo <emorylebo@gmail.com>
2022-03-02 04:07:03 +0100

    ADD image assets

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { HiMenuAlt4 } from "react-icons/hi";
 import { AiOutlineClose } from "react-icons/ai";
 
-import logo from "../images/x.jpg";
+import logo from "../images/logodarkno.png";
 
 const NavbarItem = ({ title, classProps }) => {
   return <li className={`mx-4 cursor-pointer ${classProps}`}>{title}</li>;
@@ -12,7 +12,7 @@ const Navbar = () => {
   return (
     <nav className="w-full flex md:justify-items-center justify-between items-center p-4">
       <div className="md:flex[0.5] flex-initial justify-center items-center">
-        <img src={logo} alt="logo" className="w-32 cursor-pointer" />
+        <img src={logo} alt="logo" className=" cursor-pointer" />
       </div>
       <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
         {["Market", "Exchange", "Tutorials", "Wallet"].map((item, index) => (
