3afe5cba1f870d1641e084c476382e88115e4776
Godfrey Lebo <emorylebo@gmail.com>
2022-03-02 05:50:59 +0100

    UPDATE Welcome with commonStyles with servies

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -39,7 +39,14 @@ const Welcome = () => {
             <p className="text-white text-base font-semibold">Connect Wallet</p>
           </button>
           <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
-            <div className={`rounded-tl-2xl ${commonStyles}`}></div>
+            <div className={`rounded-tl-2xl ${commonStyles}`}>Reliability</div>
+            <div className={commonStyles}>Security</div>
+            <div className={`rounded-tr-2xl ${commonStyles}`}>Solana</div>
+            <div className={`rounded-bl-2xl ${commonStyles}`}>Web 3.0</div>
+            <div className={commonStyles}>Mine EmoriCoin</div>
+            <div className={`rounded-br-2xl ${commonStyles}`}>
+              Blockchain
+            </div>
           </div>
         </div>
       </div>
