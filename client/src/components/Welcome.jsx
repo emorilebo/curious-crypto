1971134c162dc0d2351c229d96d23b0a5be1d977
Godfrey Lebo <emorylebo@gmail.com>
2022-03-02 08:31:36 +0100

    UPDATE Welcome with Sol icon

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -3,6 +3,7 @@ import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
 
 import { Loader } from "./";
+import sol from "../images/solana-sol.png";
 
 const commonStyles =
   "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
@@ -48,7 +49,17 @@ const Welcome = () => {
           </div>
         </div>
         <div className="flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10">
-          <div className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 sol-card white-glassmorphism"></div>
+          <div className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 sol-card white-glassmorphism">
+            <div className="flex justify-between flex-col w-full h-full">
+              <div className="flex justify-between items-start ">
+                <div className="w-10 h-10 rounded-full border-2 border-blue-500 flex justify-center items-center">
+                  {/* <SiEthereum fontSize={21} color="#fff" /> */}
+                  <img src={sol} alt="sol" className="h-6" />
+                </div>
+                <BsInfoCircle />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
