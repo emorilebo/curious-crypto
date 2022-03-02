d3ab284917dc248bd55a63178b5d5a46e1a50411
Godfrey Lebo <emorylebo@gmail.com>
2022-03-02 03:40:43 +0100

    UPDATE Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,12 +4,21 @@ import { AiOutlineClose } from "react-icons/ai";
 
 import logo from "../images/x.jpg";
 
+const NavbarItem = ({ title, classProps }) => {
+  return <li className={`mx-4 cursor-pointer ${classProps}`}>{title}</li>;
+};
+
 const Navbar = () => {
   return (
     <nav className="w-full flex md:justify-items-center justify-between items-center p-4">
       <div className="md:flex[0.5] flex-initial justify-center items-center">
         <img src={logo} alt="logo" className="w-32 cursor-pointer" />
       </div>
+      <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
+        {["Market", "Exchange", "Tutorials", "Wallet"].map((item, index) => (
+          <NavbarItem key={item + index} title={item} />
+        ))}
+      </ul>
     </nav>
   );
 };
