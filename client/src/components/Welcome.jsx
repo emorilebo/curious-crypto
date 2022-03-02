4f565c38085ac1fc2fb9833b789e3e190b615466
Godfrey Lebo <emorylebo@gmail.com>
2022-03-02 05:58:43 +0100

    UPDATE Welcome with sol-card

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -43,11 +43,12 @@ const Welcome = () => {
             <div className={commonStyles}>Security</div>
             <div className={`rounded-tr-2xl ${commonStyles}`}>Solana</div>
             <div className={`rounded-bl-2xl ${commonStyles}`}>Web 3.0</div>
-            <div className={commonStyles}>Mine EmoriCoin</div>
-            <div className={`rounded-br-2xl ${commonStyles}`}>
-              Blockchain
-            </div>
+            <div className={commonStyles}>EmoriCoin</div>
+            <div className={`rounded-br-2xl ${commonStyles}`}>Blockchain</div>
           </div>
+        </div>
+        <div className="flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10">
+          <div className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 sol-card white-glassmorphism"></div>
         </div>
       </div>
     </div>
