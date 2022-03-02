9e76c0a7d177df8610936cf5068653ba53c91b6d
Godfrey Lebo <emorylebo@gmail.com>
2022-03-02 05:41:20 +0100

    UPDATE Welcome with commonStyles

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -4,6 +4,20 @@ import { BsInfoCircle } from "react-icons/bs";
 
 import { Loader } from "./";
 
+const commonStyles =
+  "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
+
+// const Input = ({ placeholder, name, type, value, handleChange }) => (
+//   <input
+//     placeholder={placeholder}
+//     type={type}
+//     step="0.0001"
+//     value={value}
+//     onChange={(e) => handleChange(e, name)}
+//     className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
+//   />
+// );
+
 const Welcome = () => {
   const connectWallet = () => {};
   return (
@@ -17,12 +31,16 @@ const Welcome = () => {
             Explore the crypto world. Buy and sell cryptocurrencies easily on
             CuriosityXploring
           </p>
-          <button type="button" onClick={connectWallet}
-           className="flex flex-row justify-center items-center my-5 bg-[#295] p-3 rounded-full cursor-pointer hover:bg-[#254]"
+          <button
+            type="button"
+            onClick={connectWallet}
+            className="flex flex-row justify-center items-center my-5 bg-[#295] p-3 rounded-full cursor-pointer hover:bg-[#254]"
           >
             <p className="text-white text-base font-semibold">Connect Wallet</p>
           </button>
-          
+          <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
+            <div className={`rounded-tl-2xl ${commonStyles}`}></div>
+          </div>
         </div>
       </div>
     </div>
