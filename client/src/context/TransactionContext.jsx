0492a3d38bd3b3f217468dd168777c4812070f9b
Godfrey Lebo <emorylebo@gmail.com>
2022-03-05 10:19:47 +0100

    ADD TransactionContext for contextapi

---
+++ client/src/context/TransactionContext.jsx
@@ -0,0 +1,9 @@
+import React, {useState, useEffect} from 'react';
+import {ethers} from 'ethers';
+
+import {contractABI, contractAddress} from '../utils/constants';
+
+export const TransactionContext = React.createContext();
+
+const {ethereum} = window;
+window.ethereum =
--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,1 +1,4 @@
+import abi from './Transactions.json';
+
+export const contractABI = abi.abi;
 export const contractAddress = '0xFeda7eecEa19657892f02098990e2917Ac48f086';
