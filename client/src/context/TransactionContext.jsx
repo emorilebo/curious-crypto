8299ff6af4579958c23cc8bec5aec6e9b3a5b801
Godfrey Lebo <emorylebo@gmail.com>
2022-03-05 10:56:10 +0100

    UPDATE TransactionContext for contextapi

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -1,10 +1,26 @@
-import React, {useState, useEffect} from 'react';
-import {ethers} from 'ethers';
+import React, { useState, useEffect } from "react";
+import { ethers } from "ethers";
 
-import {contractABI, contractAddress} from '../utils/constants';
+import { contractABI, contractAddress } from "../utils/constants";
 
 export const TransactionContext = React.createContext();
 
-const {ethereum} = window;
+const { ethereum } = window;
 
-window.ethereum =
+//window.ethereum =
+
+const getEthereumContract = () => {
+  const provider = new ethers.providers.Web3Provider(ethereum);
+  const signer = provider.getSigner();
+  const transactionContract = new ethers.Contract(
+    contractABI,
+    contractAddress,
+    signer
+  );
+
+  console.log({
+    provider,
+    signer,
+    transactionContract,
+  });
+};
