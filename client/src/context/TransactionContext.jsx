35e72a8c7d1aa1cf6953fa80eefded2321adf92f
Godfrey Lebo <emorylebo@gmail.com>
2022-03-06 12:43:57 +0100

    UPDATE TransactionContext for contextapi with formdata and handlechange

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -27,6 +27,11 @@ const getEthereumContract = () => {
 
 export const TransactionProvider = ({ children }) => {
   const [currentAccount, setCurrentAccount] = useState("");
+  const [formData, setFormData] = useState({addressTo: '', amount: '', keyword: '', message: '',});
+
+  const handleChange = (e, name) => {
+    setFormData((prevState)=>({...prevState, [name]: e.target.value}))
+  }
 
   const checkIfWalletIsConnected = async () => {
     try {
@@ -59,12 +64,25 @@ export const TransactionProvider = ({ children }) => {
     }
   };
 
+  const sendTransaction = async () => {
+    try {
+      if (!ethereum) return alert("Please install metamask");
+
+      //get the data from the form
+      
+    } catch (error) {
+      console.log(error);
+      throw new Error("No ethereum object.");
+    }
+    }
+  }
+
   useEffect(() => {
     checkIfWalletIsConnected();
   }, []);
 
   return (
-    <TransactionContext.Provider value={{ connectWallet, currentAccount }}>
+    <TransactionContext.Provider value={{ connectWallet, currentAccount, formData, setFormData, handleChange }}>
       {children}
     </TransactionContext.Provider>
   );
